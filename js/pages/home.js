73a8bd1d780c136417d0c110c3dc72d146567fc4
Gabriel Nascimento <100228796+GabrielBaiano@users.noreply.github.com>
2026-01-19 02:58:24 -0300

    feat: implement initial Home page component with profile, experience, education, and interactive elements.

--- js/pages/home.js
+++ js/pages/home.js
@@ -38,7 +38,7 @@ const HomePage = {
                 <!-- Bio -->
                 <div class="bio">
                     <p>
-Welcome, i'm a Brazilian <svg class="bio-location-pin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" height="1em"><path fill="currentColor" d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"/></svg> Full-Stack Developer with over 3 years of experience, and I'm also part of the <strong>YellowHood agency</strong>.
+Welcome, i'm a Brazilian<svg class="bio-location-pin" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" height="1em"><path fill="currentColor" d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"/></svg> Full-Stack Developer with over 3 years of experience, and I'm also part of the <strong>YellowHood agency</strong>.
                     </p>
                     <br>
                     <p>
